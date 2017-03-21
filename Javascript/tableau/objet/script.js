218f89a8411894e9a9d4a155f800ed81ce64d368
Etudiant <james.bosse@lepoles.com>
2017-03-21 16:37:34 +0100

    maj

--- Javascript/tableau/objet/script.js
+++ Javascript/tableau/objet/script.js
@@ -13,3 +13,38 @@ document.write('<li>Date de l\'achat: ' + voiture.date_achat + '</li>');
 document.write('<li>Passager1: ' + voiture.passager1 + '</li>');
 document.write('<li>Passager2: ' + voiture.passager2 + '</li>');
 document.write('</ul>');
+
+
+
+
+
+
+
+
+
+
+
+const TAUX_TVA = 20.0;
+var demandeRemise;
+var montant_ht;
+var montant_ttc;
+var montant_tva;
+var pourcentage_remise;
+
+montant_ht = window.prompt('Quel est le montant HT ?');
+montant_ht = parseFloat(montant_ht);
+
+demande_remise = window.prompt('Souhaitez-vous une remise ?');
+
+if(demande_remise == "oui") {
+    pourcentage_remise = window.prompt('entrée une remise en % ');
+    pourcentage_remise = parseFloat(pourcentage_remise);
+    montant_ht = montant_ht - (montant_ht * pourcentage_remise / 100);
+    montant_tva = TAUX_TVA / 100 * montant_ht;
+    montant_ttc = montant_ht + montant_tva;
+    document.write("Le montant TTC est de " + montant_ttc + "<br />");
+    document.write(" Il y a une remise de " + pourcentage_remise + '%');
+}
+else {
+    alert("non")
+}
